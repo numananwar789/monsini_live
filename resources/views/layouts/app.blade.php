c1757ea80563f6f66ae952c4ad7dd056918151e0
Numan Anwar <numananwar789@gmail.com>
2026-07-27 21:41:17 +0500

    Use asset() helper and add vendor scripts
    Replace hardcoded bootstrap script path with Laravel's asset() helper and add vendor-all.min.js and pcoded.min.js includes in resources/views/layouts/app.blade.php to ensure correct asset URL generation and load required frontend scripts.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,7 +56,9 @@
     {{-- Global JS --}}
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
         crossorigin="anonymous"></script>
-    <script src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/vendor-all.min.js') }}"></script>
+    <script src="{{ asset('assets/js/pcoded.min.js') }}"></script>
 
     {{-- Page-specific JS --}}
     @yield('page-js')
