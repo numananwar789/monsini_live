6d5d37d45e96888bcaf6f51fdaf75173725b8616
Numan Anwar <numananwar789@gmail.com>
2026-09-26 15:02:51 +0500

    Fix mail view namespace path
    Update the mail view namespace to use the HTML mail views directory so Laravel resolves custom mail templates correctly. This also keeps the schema default string length configuration in place for compatibility.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-       Schema::defaultStringLength(191);
-       View::addNamespace('mail', resource_path('views/vendor/mail'));
+        Schema::defaultStringLength(191);
+        View::addNamespace('mail', resource_path('views/vendor/mail/html'));
     }
 }
